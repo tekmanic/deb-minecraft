539d4c04516e7df3529f90af0a1d71c4361f4e3f
tekmanic <manictek@gmail.com>
2022-02-01 07:28:50 -0500

    change gotty term

--- run/nobody/webui.sh
+++ run/nobody/webui.sh
@@ -10,7 +10,7 @@ if [[ "${ENABLE_WEBUI_CONSOLE}" == "yes" ]]; then
 
 	echo "[info] Starting Minecraft console Web UI..."
 	# note - do NOT quote the credentials, it will not start otherwise
-	gotty --port=8222 --title-format "${WEBUI_CONSOLE_TITLE}" ${credentials} --permit-write screen -x minecraft
+	gotty --port=8222 --title-format "${WEBUI_CONSOLE_TITLE}" ${credentials} --term xterm --permit-write screen -x minecraft
 
 else
 
